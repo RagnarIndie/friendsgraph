fa80a37746db20c8c3126017f7684ff293fef538
Ruslan Sazonov <ragnar.indie@gmail.com>
2015-12-21 13:05:18 +0000

    README file was added; default error handler was added

--- config/routes/all.php
+++ config/routes/all.php
@@ -7,3 +7,7 @@ require_once __DIR__.'/service/routes.php';
 
 //API routes
 require_once __DIR__.'/api/v01/routes.php'; #Version 0.1
+
+$app->error(function (\Exception $e, $code) {
+    return new \Symfony\Component\HttpFoundation\Response(json_encode(['success' => false]), 404);
+});
